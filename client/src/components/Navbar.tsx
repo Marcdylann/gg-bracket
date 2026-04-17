f7a5a0d2275b397723a5f306debfcfc33ac9c7fa
Marcdylann <dylanpilapil2@gmail.com>
2026-04-17 18:39:56 +0800

    made logo text bigger and added lifting when hovering over matches,teams, and standings

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,23 +1,60 @@
 import { Text, Flex, Button } from "@chakra-ui/react";
 import { useAuth } from "../context/AuthContext";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const { user, logout } = useAuth();
   const navigate = useNavigate();
 
   return (
-    <Flex justify="space-between" align="center" p={4} bg="gray.900">
-      <Link to ="/">GG Bracket</Link>
-      
+    <Flex
+      justify="space-between"
+      align="center"
+      p={4}
+      bg="gray.900"
+      boxShadow="lg"
+    >
+      <Link to="/">
+        <Text fontSize="3xl" fontWeight="bold">
+          GG Bracket
+        </Text>
+      </Link>
+
       <Flex gap={6}>
-        <Link to="/matches">Matches</Link>
-        <Link to="/teams">Teams</Link>
-        <Link to="/standings">Standings</Link>
+        <Link to="/matches">
+          <Text
+            _hover={{ transform: "translateY(-2px)", transition: "all 0.2s" }}
+          >
+            Matches
+          </Text>
+        </Link>
+        <Link to="/teams">
+          <Text
+            _hover={{ transform: "translateY(-2px)", transition: "all 0.2s" }}
+          >
+            Teams
+          </Text>
+        </Link>
+        <Link to="/standings">
+          <Text
+            _hover={{ transform: "translateY(-2px)", transition: "all 0.2s" }}
+          >
+            Standings
+          </Text>
+        </Link>
       </Flex>
 
       <Flex>
-        {user && <Button onClick = {()=>{logout(); navigate('login');}}>Logout</Button>}
+        {user && (
+          <Button
+            onClick={() => {
+              logout();
+              navigate("login");
+            }}
+          >
+            Logout
+          </Button>
+        )}
       </Flex>
     </Flex>
   );
