158dd7ba2d63728ff326b3679b53dc97f08e5868
Marcdylann <dylanpilapil2@gmail.com>
2026-05-07 20:54:48 +0800

    removed uneccesary functions

--- client/src/features/auth/Register.tsx
+++ client/src/features/auth/Register.tsx
@@ -19,7 +19,6 @@ const Register = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
-  const [hidden, setHidden] = useState(true);
 
   const handleRegister = async () => {
     if (password != confirmPassword) {
